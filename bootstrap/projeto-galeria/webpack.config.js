e2e219b21f9d1a75c0d9d7cbea29e6bb45768d18
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2023-03-19 00:06:48 -0300

    Refactory aula 397

--- bootstrap/projeto-galeria/webpack.config.js
+++ bootstrap/projeto-galeria/webpack.config.js
@@ -1,8 +1,8 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
@@ -14,12 +14,12 @@ module.exports = {
     },
     optimization: {
         minimizer: [
-            // new UglifyJsPlugin({
-            //     cache: true,
-            //     parallel: true,
-            //     sourceMap: true
-            // }),
-            // new OptimizeCSSAssetsPlugin({})
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
         ]
     },
     output: {
