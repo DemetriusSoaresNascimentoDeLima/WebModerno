01d20b4928206237fbb999ea1affe1f14ad34b54
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2023-03-25 15:31:39 -0300

    Aula 444

--- aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,11 +1,12 @@
 import styles from '../styles/Estiloso.module.css' 
-import Link from 'next/link'
+import Layout from '../components/Layout'
 
 export default function Estiloso() {
     return (
+        <Layout titulo="Exemplo de CSS Modularizado">
         <div className={styles.roxo}>
-            <Link href="/">Voltar</Link>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
+        </Layout>
     )
 }
