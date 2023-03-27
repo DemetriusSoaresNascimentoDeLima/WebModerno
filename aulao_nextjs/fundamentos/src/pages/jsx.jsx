eb3f33c7282241d4e697e03e6810667897fdec04
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2023-03-27 11:21:21 -0300

    Aula 445

--- aulao_nextjs/fundamentos/src/pages/jsx.jsx
+++ aulao_nextjs/fundamentos/src/pages/jsx.jsx
@@ -1,17 +1,20 @@
+import Layout from "../components/Layout"
 export default function Jsx() {
     const titulo = <h1>JSX é um conceito Central</h1>
 
     function subtitulo() {
         return <h2>{"muito legal".toUpperCase()}</h2>
-    } 
+    }
 
     return (
-        <div>
-            {titulo}
-            {subtitulo()}
-            <p>
-                {JSON.stringify({nome: 'João', idade: 30})}
-            </p>
-        </div>
+        <Layout titulo="Entendendo o JSX">
+            <div>
+                {titulo}
+                {subtitulo()}
+                <p>
+                    {JSON.stringify({ nome: 'João', idade: 30 })}
+                </p>
+            </div>
+        </Layout>
     )
 }
