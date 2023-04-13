fe00612c7cdd84695e09c6dcba21623a46a7b4ce
Demétrius Soares do Nascimento <demetriussoaresnascimento555@gmail.com>
2023-04-13 14:05:57 -0300

    Aula 509

--- vue/most-used-words/vue.config.js
+++ vue/most-used-words/vue.config.js
@@ -1,13 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		},
-    electronBuilder: {
-      nodeIntegration:true
-    }
-  }
-})
+module.exports = {
+	"transpileDependencies": [
+		"vuetify"
+	],
+	pluginOptions: {
+		electronBuilder: {
+			nodeIntegration: true
+		}
+	}    
+}
